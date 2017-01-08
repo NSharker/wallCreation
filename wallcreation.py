4f2fe564bc66996e380c5cd043f8d6e306d989fd
NSharker <nishad.sharker@gmail.com>
2017-01-07 22:20:36 -0500

    Redefined the program as a function
    -Rewrote the code to now have a wallCoordinates function
    -Need to write a parser for the data_laser.txt and data_pose.txt

--- wallcreation.py
+++ wallcreation.py
@@ -2,7 +2,7 @@ import math
 
 
 
-
+'''
 laserDataFile = open('firstDataLaser.txt','r')
 
 wallCoordinateFile = open('wallCoordinate.txt', 'w')
@@ -33,8 +33,44 @@ for laserDistance in laserDataFile.readline().split("\t"):
 
 laserDataFile.close()
 wallCoordinateFile.close()
-        
-        
+'''
+
+
+def wallCoordinates(robotXPos, robotYPos, maxDistOfLaser, startingAngle, angleIncrement, laserStride, listOfLaserReading):
+    """Returns a list of coordinates where the lasers hit the walls.
     
+    Keyword Argumenets:
+    robotXPos -- the X coordinate of the robot
+    robotYPos -- the Y coordinate of the robot
+    maxDistOfLaser -- the maximum distance of the laser
+    startingAngle -- the starting angle in radians
+    angleIncrement -- the angle increment for each laser
+    laserStride -- the stride between the lasers?, 1 includes all lasers, 2 includes every other laser and so on
+    listOfLaserReading -- the list of all the laser readings
+    """
+    
+    listOfCoordinates = []
+    currentAngleInRadian = startingAngle
+    for currentLaserReading in listOfLaserReading[::laserStride]:
+        if(float(currentLaserReading) < float(maxDistOfLaser)):
+            wallX = (float (currentLaserReading) * math.cos(currentAngleInRadian)) + robotXPos
+            wallY = (float (currentLaserReading) * math.sin(currentAngleInRadian)) + robotYPos
+            wallCoorTuple = (wallX, wallY)
+            listOfCoordinates.append(wallCoorTuple)
+        currentAngleInRadian = currentAngleInRadian + (laserStride * angleIncrement)
+        
+    return listOfCoordinates
 
 
+laserDataFile = open('firstDataLaser.txt','r')
+
+laserData = []
+
+for item in laserDataFile.readline().split("\t"):
+    laserData.append(item)
+
+listOfWallCoordinates = wallCoordinates(9.6347360611, 20.1799144745, 25, -1.91986000538, 0.00581699982285, 1, laserData)
+
+print(listOfWallCoordinates[0])
+print (len(listOfWallCoordinates))
+laserDataFile.close()
